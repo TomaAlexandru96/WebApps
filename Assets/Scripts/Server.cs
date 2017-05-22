289fd8886528de53007d263b949e7669926e3e14
Toma Alexandru <bartimaeus1073@gmail.com>
2017-05-22 14:47:55 +0100

    Added server

---
+++ Assets/Scripts/Server.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Server {
+
+	// Use this for initialization
+	void Start () {
+		NetworkServer.Listen (1073);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (NetworkServer.active) {
+			Debug.Log ("Connected");
+		}
+	}
+}
