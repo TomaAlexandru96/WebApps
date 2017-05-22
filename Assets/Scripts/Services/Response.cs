1366ab87012ac60e7ca6a448acf04294670559a0
Toma Alexandru <bartimaeus1073@gmail.com>
2017-05-22 19:11:06 +0100

    Added template for DB Service

---
+++ Assets/Scripts/Services/Response.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Response {
+	private String message;
+	private Status status;
+
+	public Response (String message, Status status) {
+		this.message = message;
+		this.status = status;
+	}
+
+	public Response (Status status) {
+		this.message = "";
+		this.status = status;
+	}
+
+	public String getMessage () {
+		return message;
+	}
+
+	public Status getStatus () {
+		return status;
+	}
+
+	public override string ToString ()
+	{
+		return "Message: " + message + " Status: " + status;
+	}
+}
+
