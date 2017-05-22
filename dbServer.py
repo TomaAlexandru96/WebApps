0e8f9b0678d5f5a6d3d3f003a5cc055e673359be
Alexandru Toma <alexandru.toma15@imperial.ac.uk>
2017-05-22 23:33:15 +0100

    Added psql

--- dbServer.py
+++ dbServer.py
@@ -1,4 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
+
+import psycopg2
  
 from http.server import BaseHTTPRequestHandler, HTTPServer
  
@@ -33,5 +35,26 @@ def run():
   print('running server...')
   httpd.serve_forever()
  
+
+def runPSQLQuery():
+  try:
+    connect_str = "dbname='g1627107_u' user='g1627107_u' host='db.doc.ic.ac.uk'" + \
+                  "password='X44dCYOeJe'"
+    # establish connection
+    conn = psycopg2.connect(connect_str)
+    # create a psycopg2 cursor that can execute queries
+    cursor = conn.cursor()
+
+    # create a new table with a single column called "name"
+    #cursor.execute("""CREATE TABLE tutorials (name char(40));""")
+    # run a SELECT statement - no data in there, but we can try it
+    #cursor.execute("""SELECT * from tutorials""")
+    #rows = cursor.fetchall()
+    #print(rows)
+  except Exception as e:
+    print("Can't connect to psql!")
+    print(e)
  
-run()
+# run()
+runPSQLQuery()
+
