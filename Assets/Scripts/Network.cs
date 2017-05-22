253ee125da2e536ad38d1994686a19eca1c56597
Toma Alexandru <bartimaeus1073@gmail.com>
2017-05-22 16:52:33 +0100

    Added Photon

---
+++ Assets/Scripts/Network.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Network : MonoBehaviour {
+	public void Start() {
+		Debug.Log("Start");
+		PhotonNetwork.ConnectUsingSettings("v0.01");
+	}
+
+	void OnGUI()
+	{
+		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+	}
+}
