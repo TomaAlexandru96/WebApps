ac9c2e0239f01754445ce4e77117ef39ca1772c0
Alexandru Toma <alexandru.toma15@imperial.ac.uk>
2017-05-23 02:03:20 +0100

    Added template for GET, POST methods including psql access

--- dbServer.py
+++ dbServer.py
@@ -6,9 +6,45 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
  
 class DBHTTPHandler(BaseHTTPRequestHandler):
 
-  # PPOST
+  conn = None
+
+
+  def connectToDB(self):
+    if self.conn == None:
+      try:
+        connect_str = "dbname='g1627107_u' user='g1627107_u'" + \
+                      "host='db.doc.ic.ac.uk' password='X44dCYOeJe'"
+        # establish connection
+        self.conn = psycopg2.connect(connect_str)
+        return self.conn
+      except Exception as e:
+        print("Can't connect to psql!")
+        print(e)
+        return None
+    return self.conn
+
+  
+  def run_query(self, message): 
+    # create a psycopg2 cursor that can execute queries
+    cursor = self.connectToDB().cursor()
+    cursor.execute(message)
+    return cursor
+
+
+  # POST
   def do_POST(self):
-    print("Hello")
+    # Send response status code
+    self.send_response(200)
+    
+    # Send headers
+    self.send_header('Content-type','text/html')
+    self.end_headers()
+    
+    # Send message back to client
+    message = "You send a post request!"
+    # Write content as utf-8 data
+    self.wfile.write(bytes(message, "utf8"))
+    return
 
  
   # GET
@@ -21,13 +57,13 @@ class DBHTTPHandler(BaseHTTPRequestHandler):
     self.end_headers()
     
     # Send message back to client
-    message = "Hello world!"
+    message = "You send a get request!"
     # Write content as utf-8 data
     self.wfile.write(bytes(message, "utf8"))
     return
 
  
-def run():
+def startServer():
   print('starting server...')
  
   server_address = ('146.169.46.104', 8081)
@@ -36,25 +72,6 @@ def run():
   httpd.serve_forever()
  
 
-def runPSQLQuery():
-  try:
-    connect_str = "dbname='g1627107_u' user='g1627107_u' host='db.doc.ic.ac.uk'" + \
-                  "password='X44dCYOeJe'"
-    # establish connection
-    conn = psycopg2.connect(connect_str)
-    # create a psycopg2 cursor that can execute queries
-    cursor = conn.cursor()
-
-    # create a new table with a single column called "name"
-    #cursor.execute("""CREATE TABLE tutorials (name char(40));""")
-    # run a SELECT statement - no data in there, but we can try it
-    #cursor.execute("""SELECT * from tutorials""")
-    #rows = cursor.fetchall()
-    #print(rows)
-  except Exception as e:
-    print("Can't connect to psql!")
-    print(e)
- 
-# run()
-runPSQLQuery()
+if __name__ == "__main__":
+  startServer()
 
