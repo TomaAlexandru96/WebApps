18cc86a2089c2b7de793aca593e907fcba4456e8
Vinamra Agrawal <va1215@ic.ac.uk>
2017-05-24 14:13:46 +0100

    add menu script

---
+++ Assets/Scripts/MenuScene/TopUIPanelController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopUIPanelController : MonoBehaviour {
+
+	public GameObject MenuPanel;
+	public GameObject LeaderBoard;
+	public GameObject PlayerInfo;
+
+	/* Used by login button to change the pane to Menu pane */
+	public void ActivateMenu () {
+		MenuPanel.SetActive (true);
+		LeaderBoard.SetActive (false);
+		PlayerInfo.SetActive (false);
+	}
+
+	/* Used by login button to change the pane to LeaderBoard pane */
+	public void ActivateLeaderBoard () {
+		MenuPanel.SetActive (false);
+		LeaderBoard.SetActive (true);
+		PlayerInfo.SetActive (false);
+	}
+
+	/* Used by login button to change the pane to PlayerInfo pane */
+	public void ActivatePlayerInfo () {
+		MenuPanel.SetActive (false);
+		LeaderBoard.SetActive (false);
+		PlayerInfo.SetActive (true);
+	}
+}
