9fb880ca87b3a0f073faa4e84908fc5237cd2ef6
Vinamra Agrawal <va1215@ic.ac.uk>
2017-05-28 14:35:40 +0100

    request accept and delete UI added

---
+++ Assets/Scripts/MenuScene/FriendsManager/FriendsPanelManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FriendsPanelManager : MonoBehaviour {
+
+	private GameObject friendsPanelContent;
+	public GameObject friendsEntry;
+	public GameObject friendRequestEntry;
+
+	public void Start() {
+		friendsPanelContent = GameObject.FindGameObjectWithTag ("Friends").transform.GetChild (1).GetChild(0).GetChild(0).gameObject;
+	}
+
+	public void AcceptRequest() {
+		GameObject newFriendEntry = Instantiate (friendsEntry, Vector3.zero, Quaternion.identity);
+		newFriendEntry.transform.SetParent (friendsPanelContent.transform, false);
+		string name = friendRequestEntry.transform.GetChild (0).GetComponent<Text> ().text;
+		newFriendEntry.transform.GetChild (0).GetComponent<Text> ().text = name;
+		GameObject image = Instantiate (friendRequestEntry.transform.GetChild (1).gameObject, Vector3.zero, Quaternion.identity);
+		Destroy (newFriendEntry.transform.GetChild (1).gameObject);
+		image.transform.SetParent (newFriendEntry.transform, false);
+		DestroyImmediate (friendRequestEntry);
+	}
+
+	public void RejectRequest() {
+		DestroyImmediate (friendRequestEntry);
+	}
+
+}
