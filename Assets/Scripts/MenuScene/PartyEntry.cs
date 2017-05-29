67ed7e5493ffb96793b36e113642c99a6da6e089
Toma Alexandru <bartimaeus1073@gmail.com>
2017-05-29 20:04:18 +0100

    Fixed party name

---
+++ Assets/Scripts/MenuScene/PartyEntry.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PartyEntry : MonoBehaviour {
+
+	public Text usernameText;
+
+	public void ChangeName (string username) {
+		usernameText.text = username;
+	}
+}
