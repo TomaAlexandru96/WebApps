f962210750f70fde8a8fd361fefc64323a686d06
SilviaSapora <silvia.sapora@gmail.com>
2017-05-30 19:12:41 +0100

    Adventure enemy

---
+++ Assets/Scripts/AdventureScene/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using UnityEngine;
+
+public class Player {
+
+	public PlayerStats stats;
+	public int currentHP;
+	public int exp;
+
+	public Player() {
+
+		stats = new PlayerStats (PlayerType.FrontEndDev);
+		currentHP = stats.maxHP;
+		exp = 0;
+	}
+
+}
+
---
+++ Assets/Scripts/AdventureScene/PlayerStats.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+public class PlayerStats {
+
+	public int level;
+	public int maxHP;
+	public int javascript;
+	public int html;
+	public int python;
+	public int java;
+	public int mySQL;
+	public int git;
+
+	public const int increaseLevelUp = 3;
+
+	public PlayerStats(PlayerType type) {
+		level = 1;
+		maxHP = 20;
+		switch (type) {
+		case PlayerType.BackEndDev: 
+			javascript = 8;
+			html = 8;
+			python = 2;
+			java = 2;
+			mySQL = 2;
+			git = 5;
+			break;
+		case PlayerType.FrontEndDev:
+			javascript = 2;
+			html = 2;
+			python = 8;
+			java = 8;
+			mySQL = 8;
+			git = 5;
+			break;
+		case PlayerType.FullStackDev:
+			javascript = 5;
+			html = 5;
+			python = 5;
+			java = 5;
+			mySQL = 5;
+			git = 5;
+			break;
+		}
+	}
+
+	public void LevelUp() {
+		level++;
+		maxHP += increaseLevelUp;
+		javascript += increaseLevelUp;
+		html += increaseLevelUp;
+		python += increaseLevelUp;
+		java += increaseLevelUp;
+		mySQL += increaseLevelUp;
+		git += increaseLevelUp;
+
+	}
+
+}
+
