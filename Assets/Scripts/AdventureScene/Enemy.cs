4bd8fe4c97acb38f08fb692b1b99174b8761f3b5
Silvia Sapora <silvia.sapora@google.com>
2017-05-30 19:12:41 +0100

    Adventure enemy

---
+++ Assets/Scripts/AdventureScene/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public int playerDamage;
+	public Transform target;
+	public float speed;
+	public int damage;
+
+	// Use this for initialization
+	void Start () {
+		speed = 0.5f;
+		target = GameObject.FindGameObjectWithTag ("Player").transform;
+		damage = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		MoveEnemy ();
+	}
+
+	public void MoveEnemy () {
+		transform.LookAt(target);
+		transform.Rotate(new Vector3(0,-90,-90), Space.Self);
+
+		//only move if I'm far away from the target
+		if (Vector2.Distance(transform.position,target.position)>1f) {
+			transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+		}
+	}
+}
---
+++ Assets/Scripts/AdventureScene/PlayerLife.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLife : MonoBehaviour {
+
+	public int totHP;
+	public int curHP;
+	public P1_MoveAnim player;
+	public float scale;
+	public float currentScale;
+
+	// Use this for initialization
+	void Start () {
+		player = transform.parent.gameObject.GetComponent<P1_MoveAnim> ();
+		scale = transform.localScale.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		totHP = player.totHP;
+		curHP = player.curHP;
+		currentScale = curHP;
+		transform.localScale = new Vector3(currentScale,1f,1f);
+	}
+}
