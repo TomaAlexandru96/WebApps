b97c233e0e84ed7417946947921f20e007780449
Vinamra Agrawal <va1215@ic.ac.uk>
2017-05-30 02:04:41 +0100

    friends option UI implemented

---
+++ Assets/Scripts/MenuScene/FriendsManager/FriendsEntry.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FriendsEntry : MonoBehaviour {
+
+	public GameObject friendsPanel;
+	private GameObject optionPanel;
+
+	public void Start() {
+		optionPanel = GameObject.FindGameObjectWithTag ("Menu").transform.GetChild(5).gameObject;
+		optionPanel.SetActive (false);
+	}
+
+	public void ShowOptions() {
+		if (optionPanel.GetActive ()) { 
+			optionPanel.SetActive (false);
+		} else {
+			optionPanel.SetActive (true);
+			Vector3 friendPos = friendsPanel.transform.position;
+			Vector3 OptionPos = optionPanel.transform.position;
+			optionPanel.transform.position = new Vector3 (OptionPos.x, friendPos.y-30, OptionPos.z);
+		}
+	}
+}
