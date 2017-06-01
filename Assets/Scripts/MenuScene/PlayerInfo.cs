e46d00507679a647525c40048107a5b7adea9239
SilviaSapora <silvia.sapora@gmail.com>
2017-06-01 19:12:30 +0100

    Player info panel fixed

---
+++ Assets/Scripts/MenuScene/PlayerInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInfo : MonoBehaviour {
+
+	public Text username;
+	public Image avatar;
+
+	// Use this for initialization
+	void Start () {
+		username.text = "Name: " + CurrentUser.GetInstance ().GetUserInfo ().username;
+		avatar.sprite = CurrentUser.GetInstance ().GetUserInfo ().character.GetImage ();
+	}
+}
