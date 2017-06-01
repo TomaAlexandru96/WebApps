80f696601cedb340cb5987de272f6bbd28dbebfb
Vinamra Agrawal <va1215@ic.ac.uk>
2017-06-01 10:34:58 +0100

    auto select in login added

--- Assets/Scripts/LoginScene/MainPanelController.cs
+++ Assets/Scripts/LoginScene/MainPanelController.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainPanelController : MonoBehaviour {
 	
 	public GameObject loginPanel;
 	public GameObject registerPanel;
+	public InputField userNameField;
+	public InputField userNameFieldRegister;
 
 	public void Update () {
 		if (CurrentUser.GetInstance ().IsLoggedIn ()) {
@@ -18,11 +21,13 @@ public class MainPanelController : MonoBehaviour {
 	public void Login () {
 		this.gameObject.SetActive (false);
 		loginPanel.GetComponent<LoginPanelController> ().Activate (true);
+		userNameField.Select ();
 	}
 
 	/* Used by registeer button to change the pane to register pane */
 	public void Register () {
 		this.gameObject.SetActive (false);
 		registerPanel.GetComponent<RegisterPanelController> ().Activate (true);
+		userNameFieldRegister.Select ();
 	}
 }
