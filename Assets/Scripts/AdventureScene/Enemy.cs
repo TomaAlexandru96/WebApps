f3339b81b19435e06c5a73dfb23aaf1520d3f4be
SilviaSapora <silvia.sapora@gmail.com>
2017-06-04 20:47:41 +0100

    A* pathfinding for Enemies added

--- Assets/Scripts/AdventureScene/Enemy.cs
+++ Assets/Scripts/AdventureScene/Enemy.cs
@@ -15,27 +15,75 @@ public class Enemy : MonoBehaviour {
 	public float actionTime = 1f;
 	public float nextAction = 0;
 	public Animator animator;
+	public Grid grid;
+	public bool targetInRange;
+	public Point lastTargetPos;
+	public BreadCrumb currentBr;
 
 
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator> ();
 		rb = GetComponent<Rigidbody2D> ();
+		grid = GetComponentInParent<Grid> ();
 		speed = 0.5f;
+		targetInRange = false;
 		target = GameObject.FindGameObjectWithTag ("Player").transform;
 		SetDamage ();
 		SetMaxHP ();
 		curHP = maxHP;
+		lastTargetPos = new Point((int)(target.localPosition.x*2), (int)(target.localPosition.y*2));
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (coll.gameObject.tag == "Player") {
+			targetInRange = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D coll) {
+		if (coll.gameObject.tag == "Player") {
+			targetInRange = false;
+		}
 	}
 
 	// Update is called once per frame
 	void Update () {
-		Rotate ();
+		// pathfinding
+		if (targetInRange) {
+			Point cur = new Point((int)(target.localPosition.x*2), (int)(target.localPosition.y*2));
+			if (cur != lastTargetPos) {
+				lastTargetPos = cur;
+
+				Point targetPos = new Point ((int)(target.localPosition.x * 2), (int)(target.localPosition.y * 2));
+
+				if (targetPos != null) {
+					Point enemyPos = new Point ((int)(transform.localPosition.x * 2), (int)(transform.localPosition.y * 2));
+
+					BreadCrumb currentBr = PathFinder.FindPath (grid, enemyPos, targetPos);
+				}
+			} else {
+				if (currentBr != null) {
+					if (transform.position.x - (currentBr.position.X * 0.5f) + transform.position.x > 1
+					    && transform.position.y - (currentBr.position.Y * 0.5f) + transform.position.y > 1) {
+						Vector3 movement = 
+							(new Vector3 (target.position.x - transform.position.y, target.position.y - transform.position.y, 0)).normalized * speed;
+						rb.velocity = movement;
+					} else {
+						currentBr = currentBr.next;
+					}
+				}
+			}
+		}
+
+
+		// Old movement
+		//Rotate ();
 		//only move if I'm far away from the target
-		if (Vector2.Distance (transform.position, target.position) > 0.5f
+		/*if (Vector2.Distance (transform.position, target.position) > 0.5f
 			&& !attackingPlayer) {
 			MoveEnemy ();
-		}
+		}*/
 	}
 
 	void OnCollisionEnter2D(Collision2D coll) {
