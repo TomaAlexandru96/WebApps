3f0c9ea51326752507eeecd28b140b2047338d32
Toma Alexandru <bartimaeus1073@gmail.com>
2017-06-05 16:49:22 +0100

    Added loading screen

--- Assets/Scripts/AdventureScene/UI/AdventureController.cs
+++ Assets/Scripts/AdventureScene/UI/AdventureController.cs
@@ -5,8 +5,38 @@ using UnityEngine.SceneManagement;
 
 public class AdventureController : MonoBehaviour {
 
+	public GameObject loadingScreen;
+	private HashSet<string> loadedPlayers;
+
+	public void Awake () {
+		loadedPlayers = new HashSet<string> ();
+		loadedPlayers.Add (CurrentUser.GetInstance ().GetUserInfo ().username);
+
+		UpdateService.GetInstance ().Subscribe (UpdateType.GameLoaded, (sender, message) => {
+			loadedPlayers.Add (sender);
+			if (AllPartyUsersLoaded ()) {
+				StartGame ();
+			}
+		});
+	}
+
+	public void StartGame () {
+		loadingScreen.SetActive (false);
+	}
+
 	public void Start () {
 		GameObject.FindGameObjectWithTag ("Chat").GetComponent<ChatController> ().InitDefaultChat ();
+		UpdateService.GetInstance ().SendUpdate (CurrentUser.GetInstance ().GetUserInfo ().party.partyMembers, 
+					UpdateService.CreateMessage (UpdateType.GameLoaded));
+	}
+
+	public bool AllPartyUsersLoaded () {
+		foreach (var user in CurrentUser.GetInstance ().GetUserInfo ().party.partyMembers) {
+			if (!loadedPlayers.Contains (user)) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	public void ExitGame () {
--- Assets/Scripts/Services/UpdateType.cs
+++ Assets/Scripts/Services/UpdateType.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
 public enum UpdateType {
-	UserUpdate, LoginUser, LogoutUser, PartyLeft, PartyRequest, PartyRequestAccept, ChatRequest, ChatRequestAccept, RefreshLobby
+	UserUpdate, LoginUser, LogoutUser, PartyLeft, PartyRequest, PartyRequestAccept, 
+		ChatRequest, ChatRequestAccept, RefreshLobby, GameLoaded
 }
 
