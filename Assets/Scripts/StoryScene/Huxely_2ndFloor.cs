96c41e24788a1c89b3e2e333fd8b15712e36c985
Vinamra Agrawal <va1215@ic.ac.uk>
2017-06-07 20:18:53 +0100

    Story mode reception conversation complete

--- Assets/Scripts/StoryScene/Huxely_2ndFloor.cs
+++ Assets/Scripts/StoryScene/Huxely_2ndFloor.cs
@@ -16,8 +16,8 @@ public class Huxely_2ndFloor : MonoBehaviour {
 		text [0] = "Welcome to               Imperial College London...        (press space key to continue)";
 		text [1] = "Today is your interview day!            Hope you are not too nervous ";
 		text [2] = "This to Huxley!               Most of your time in imperial will be spend here...";
-		text [3] = "Go And Talk to receptionist to find out about the next steps.";
-		directionPanel = GameObject.FindGameObjectsWithTag ("DirectionPanel")[0];
+		text [3] = "Go And Talk to receptionist to find out about the next steps.                              You Can move using W S A D";
+		directionPanel = GameObject.FindGameObjectsWithTag ("Canvas")[0].transform.GetChild(0).gameObject;
 		directionPanel.SetActive (true);
 		directionPanel.transform.GetComponent<DirectionPanel> ().DisplayText (this.text[0]);
 		index = 1;
@@ -25,14 +25,12 @@ public class Huxely_2ndFloor : MonoBehaviour {
 	}
 
 	public void Update() {
-		if (Input.anyKey && index < maxLen && (DateTime.Now - dateTime).Seconds > 1){
-			Debug.Log ("Diplay text please");
+		if (Input.GetKeyDown("space") && index < maxLen && (DateTime.Now - dateTime).Seconds > 0.5){
 			directionPanel.transform.GetComponent<DirectionPanel> ().DisplayText (this.text[index]);
 			dateTime = DateTime.Now;
 			index++;
 		}
 		if (index >= maxLen && (DateTime.Now - dateTime).Seconds > 1) {
-			Debug.Log ("Hahahah");
 			directionPanel.SetActive (false);
 			dateTime = DateTime.MaxValue;
 		}
--- Assets/Scripts/StoryScene/SpecifyMovementScript.cs
+++ Assets/Scripts/StoryScene/SpecifyMovementScript.cs
@@ -16,6 +16,9 @@ public class SpecifyMovementScript : MonoBehaviour {
 
 	public void Start () {
 		dateTime = DateTime.MinValue;
+		directionPanel = GameObject.FindGameObjectsWithTag ("Canvas")[0].transform.GetChild(0).gameObject;
+//		text[0] = "Receptionist:   Hi There,                       How Can I help You?";
+//		text[1] = "uhhh.... I am prospective student, where should I go?";
 	}
 
 	void OnTriggerEnter2D(Collider2D coll) {
@@ -29,12 +32,14 @@ public class SpecifyMovementScript : MonoBehaviour {
 
 
 	public void Update () {
-		if (inside && index < maxSize &&  (DateTime.Now - dateTime).Seconds > 1) {
+		if (Input.GetKeyDown("space") && inside && index < maxSize &&  (DateTime.Now - dateTime).Seconds > 0.5) {
 			Debug.Log("printitng + " + text [index]);
+			directionPanel.SetActive (true);
+			dateTime = DateTime.Now;
 			directionPanel.transform.GetComponent<DirectionPanel> ().DisplayText (text[index]);
 			index++;
 		}
-		if (index >= maxSize && (DateTime.Now - dateTime).Seconds > 1) {
+		if (inside && index >= maxSize && (DateTime.Now - dateTime).Seconds > 1) {
 			directionPanel.SetActive (false);
 			dateTime = DateTime.MaxValue;
 		}
