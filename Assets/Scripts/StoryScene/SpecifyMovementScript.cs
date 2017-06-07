cbad11c73996085ca4846c4187368d69ad795104
unknown <isailaandrei.i@gmail.com>
2017-06-07 15:22:49 +0100

    Integrate changes to include new added scene

---
+++ Assets/Scripts/StoryScene/SpecifyMovementScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecifyMovementScript : MonoBehaviour {
+
+
+	public GameObject SpecifyMovementPanel;
+
+	public void Start () {
+		SpecifyMovementPanel.SetActive (false);
+
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		SpecifyMovementPanel.SetActive (true);
+	}
+
+	void OnTriggerExit2D(Collider2D coll) {
+		SpecifyMovementPanel.SetActive (false);
+
+	}
+
+
+	public void Update () {
+
+	}
+}
