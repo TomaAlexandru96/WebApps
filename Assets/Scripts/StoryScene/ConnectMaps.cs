596f7da6bbb0a7a879c07c774461a12d39d17cbc
unknown <isailaandrei.i@gmail.com>
2017-06-07 20:34:24 +0100

    Add colliders to huxley 3rdfloor entrance and connected 2nFloor to 3rdFloor

---
+++ Assets/Scripts/StoryScene/ConnectMaps.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectMaps : MonoBehaviour {
+
+	public GameObject thisScene;
+	public GameObject nextScene;
+
+	public void Start () {
+		thisScene.SetActive (true);
+		nextScene.SetActive (false);
+
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+
+		thisScene.SetActive (false);
+		nextScene.SetActive (true);
+
+	}
+		
+
+
+}
