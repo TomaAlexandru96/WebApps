b951b3c4b39c5b40ff6dffad01387703c6c6b8e7
Vinamra Agrawal <va1215@ic.ac.uk>
2017-06-09 13:42:06 +0100

    Toni Script fixed

--- Assets/Scripts/StoryScene/SeatingArea.cs
+++ Assets/Scripts/StoryScene/SeatingArea.cs
@@ -7,12 +7,13 @@ public class SeatingArea : MonoBehaviour {
 
 	private Transform toni;
 
-	void Start() {
+	public void Start() {
 		toni = GameObject.FindGameObjectWithTag ("ToniCollider").transform;
 	}
 
 	void OnTriggerEnter2D(Collider2D coll) {
-		toni.GetComponent<ToniScript> ().seated = true;
+		toni = GameObject.FindGameObjectWithTag ("ToniCollider").transform;
+		toni.GetComponent<ToniScript> ().Seated();
 	}
 
 	void OnTriggerExit2D(Collider2D coll) {
