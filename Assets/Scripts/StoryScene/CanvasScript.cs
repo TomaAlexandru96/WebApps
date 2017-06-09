4bae10650adfc8bb79b03a3bb6943a5b920b60af
Vinamra Agrawal <va1215@ic.ac.uk>
2017-06-09 12:45:10 +0100

    added students and random comments

---
+++ Assets/Scripts/StoryScene/CanvasScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasScript : MonoBehaviour {
+
+	float time;
+	private bool event1;
+	private bool event2;
+
+	void Start () {
+		time = Time.time;
+		event1 = true;
+		event2 = true;
+	}
+
+	void Update () {
+
+		if (event1 && Time.time > time) {
+			event1 = false;
+			GameObject.FindGameObjectWithTag ("Chat").SetActive (false);
+			GameObject.FindGameObjectWithTag ("PlayerAbilities").SetActive (false);
+			GameObject.FindGameObjectWithTag ("DirectionPanel").SetActive (false);
+		}
+
+		if (event2 && Time.time - time > 3) {
+			event2 = false;
+			GameObject.FindGameObjectWithTag ("PartMention").SetActive (false);
+			GameObject.FindGameObjectWithTag ("Canvas").transform.GetChild (0).gameObject.SetActive (true);
+		}
+	}
+}
