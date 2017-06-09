1c8de1379195bf756ce29f6319892fda99a6fdfc
unknown <isailaandrei.i@gmail.com>
2017-06-09 20:44:55 +0100

    Basic interview question integration

---
+++ Assets/LaunchInterview.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchInterview : MonoBehaviour {
+
+	public GameObject interviewPanel;
+
+	public void Start () {
+		interviewPanel.SetActive (false);
+	}
+	void OnTriggerEnter2D(Collider2D coll) {
+		interviewPanel.SetActive (true);
+	}
+
+	void OnTriggerExit2D(Collider2D coll) {
+		interviewPanel.SetActive (true);}
+}
