6dafaa0bf17d3006d6aa3a38c6bc0b470245aba9
Toma Alexandru <bartimaeus1073@gmail.com>
2017-06-12 16:46:45 +0100

    Added random room generation

---
+++ Assets/Scripts/EndlessScene/DungeonGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* 
+ * All credits for this algorithm are given to 
+ * http://www.gamasutra.com/blogs/AAdonaac/20150903/252889/Procedural_Dungeon_Generation_Algorithm.php
+*/
+public class DungeonGenerator : MonoBehaviour {
+
+	public Vector2 ellipseSize;
+	public int maxRoomWidth;
+	public int maxRoomHeight;
+	public int minRoomWidth;
+	public int minRoomHeight;
+	public int numberOfInitialGeneratedRooms;
+	public int numberOfRooms;
+	public GameObject roomPrefab;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (GenerateInitialRooms ());
+	}
+
+	private IEnumerator GenerateInitialRooms () {
+		// startup time
+		yield return new WaitForSeconds (0.5f);
+		for (int i = 0; i < numberOfInitialGeneratedRooms; i++) {
+			GenerateRoom ();
+			yield return new WaitForSeconds (0.01f);
+		}
+	}
+
+	public Vector2 GetRandomPointInEllipse () {
+		float t = 2 * Mathf.PI * Random.value;
+		float u = Random.value + Random.value;
+		float r = u > 1 ? 2 - u : u;
+
+		return new Vector2 (ellipseSize.x * r * Mathf.Cos (t) / 2, ellipseSize.y * r * Mathf.Sin (t) / 2);
+	}
+
+	public void GenerateRoom () {
+		Vector2 position = GetRandomPointInEllipse ();
+		GameObject room = Instantiate (roomPrefab);
+		room.GetComponent <RoomSetup> ().Init (position, Random.Range(minRoomWidth, maxRoomWidth), Random.Range (minRoomHeight, maxRoomHeight), transform);
+	}
+}
---
+++ Assets/Scripts/EndlessScene/RoomSetup.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomSetup : MonoBehaviour {
+
+	public float outlineBorderSize;
+	public GameObject tilePrefab;
+	public GameObject outline;
+
+	public void Init (Vector2 position, int width, int height, Transform parent) {
+		transform.SetParent (parent);
+		Vector3 oldPos = transform.localPosition;
+		oldPos.x = position.x;
+		oldPos.y = position.y;
+		transform.localPosition = oldPos;
+
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				GameObject tile = Instantiate (tilePrefab);
+				tile.transform.SetParent (transform);
+				Vector3 pos = tilePrefab.transform.localPosition;
+
+				pos.x = tile.GetComponent<RectTransform> ().rect.width * x;
+				pos.y = tile.GetComponent<RectTransform> ().rect.height * y;
+
+				tile.transform.localPosition = pos;
+			}
+		}
+
+		Vector3 scale = outline.transform.localScale;
+		scale.x = width + outlineBorderSize;
+		scale.y = height + outlineBorderSize;
+		outline.transform.localScale = scale;
+
+		Vector3 outlinePos = outline.transform.localPosition;
+		outlinePos.x = tilePrefab.GetComponent<RectTransform> ().rect.width * width / 2 - tilePrefab.GetComponent<RectTransform> ().rect.width / 2;
+		outlinePos.y = tilePrefab.GetComponent<RectTransform> ().rect.height * height / 2 - tilePrefab.GetComponent<RectTransform> ().rect.height / 2;
+		outline.transform.localPosition = outlinePos;
+	}
+}
