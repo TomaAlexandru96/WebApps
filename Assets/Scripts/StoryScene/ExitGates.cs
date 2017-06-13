d4897bad3eef3e0126cce6f5de82ab5a2b95d522
Vinamra Agrawal <va1215@ic.ac.uk>
2017-06-13 14:02:19 +0100

    story exit structure

---
+++ Assets/Scripts/StoryScene/ExitGates.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGates : MonoBehaviour {
+
+	public bool exit;
+
+	public void Start() {
+		exit = false;
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		Exit ();
+	}
+
+	private void Exit() {
+		
+	}
+}
