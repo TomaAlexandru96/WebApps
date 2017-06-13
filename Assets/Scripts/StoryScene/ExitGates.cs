379af23bc0d48d13d782f9cacf70de47354e3cb8
Vinamra Agrawal <va1215@ic.ac.uk>
2017-06-13 14:27:52 +0100

    exit done

--- Assets/Scripts/StoryScene/ExitGates.cs
+++ Assets/Scripts/StoryScene/ExitGates.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class ExitGates : MonoBehaviour {
 
 	public bool exit;
+	public GameObject partMention;
+	public GameObject floor;
+
 
 	public void Start() {
 		exit = false;
 	}
 
 	void OnTriggerEnter2D(Collider2D coll) {
-		Exit ();
+		if (exit) {
+			Exit ();
+		}
 	}
 
 	private void Exit() {
-		
+		floor.SetActive (false);
 	}
 }
