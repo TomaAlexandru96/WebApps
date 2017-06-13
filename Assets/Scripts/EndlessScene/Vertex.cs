a6140cbb06fa6bb4b5c21de3b49a4c71bfbd953b
Toma Alexandru <bartimaeus1073@gmail.com>
2017-06-13 18:41:47 +0100

    Change structure of graph

--- Assets/Scripts/EndlessScene/Vertex.cs
+++ Assets/Scripts/EndlessScene/Vertex.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Vertex {
 
 	public Vector2 point;
-	private List<Vertex> connections = new List<Vertex> ();
 
 	public Vertex (float p1, float p2) {
 		this.point = new Vector2 (p1, p2);
@@ -16,23 +15,11 @@ public class Vertex {
 	}
 
 	public override string ToString () {
-		return point.ToString () + " conns: " + connections.Count;
+		return point.ToString ();
 	}
 
 	public override int GetHashCode () {
 		return base.GetHashCode ();
-	}
-
-	public void AddConnection (Vertex v) {
-		connections.Add (v);
-	}
-
-	public bool ConnContains (Vertex v) {
-		return connections.Contains (v);
-	}
-
-	public List<Vertex> GetConnections () {
-		return connections;
 	}
 
 	public override bool Equals (object obj) {
@@ -43,5 +30,9 @@ public class Vertex {
 		Vertex other = (Vertex) obj;
 
 		return other.point.Equals (point);
+	}
+
+	public static float GetDistance (Vertex a, Vertex b) {
+		return Vector2.Distance (a.point, b.point);
 	}
 }
