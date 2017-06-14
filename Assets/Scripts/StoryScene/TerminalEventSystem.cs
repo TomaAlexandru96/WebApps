b2afaf02c50bc56d833e9e0b4f9eeb85fbba7fd9
Vinamra Agrawal <va1215@ic.ac.uk>
2017-06-14 21:37:26 +0100

    terminal implemented

---
+++ Assets/Scripts/StoryScene/TerminalEventSystem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TerminalEventSystem : MonoBehaviour {
+
+	private EventSystem es;
+	private Transform content;
+
+	public GameObject terminalEntries;
+
+	void Start () {
+		es = GetComponent<EventSystem> ();
+		SelectNextItem ();
+		content = terminalEntries.transform.GetChild (0);
+	}
+
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.Return)) {
+			terminalEntries.transform.GetComponent<Terminal> ().ExecuteCommand (GetLastEntry ());
+			SelectNextItem ();
+		}
+	}
+
+	private void SelectNextItem () {
+		terminalEntries.transform.GetComponent<Terminal> ().CreateNewLine ();
+		es.SetSelectedGameObject (GetLastEntry().gameObject);
+	}
+
+	private Transform GetLastEntry() {
+		content = terminalEntries.transform.GetChild (0);
+		int totalEntry = content.childCount;
+		Debug.Log (totalEntry);
+		return content.GetChild (totalEntry - 1).GetChild(1);
+	}
+}
