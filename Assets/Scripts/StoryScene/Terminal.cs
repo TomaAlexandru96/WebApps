833412caf8119ea6a28aa08deacac9e5c254baf1
Vinamra Agrawal <va1215@ic.ac.uk>
2017-06-14 21:37:26 +0100

    terminal implemented

---
+++ Assets/Scripts/StoryScene/Terminal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Terminal : MonoBehaviour {
+
+	public GameObject terminalEntry;
+	public int numberOfEntries = 0;
+
+	public void ExecuteCommand(Transform command) {
+		Debug.Log (command.GetComponent<InputField>().text);
+	}
+
+	public void CreateNewLine() {
+		GameObject entry = GameObject.Instantiate (terminalEntry, new Vector3(10f,160f-(20*numberOfEntries),0f), Quaternion.identity);
+		entry.transform.SetParent (transform.GetChild(0), false);
+		if (numberOfEntries > 10) {
+			transform.GetChild (0).GetComponent<RectTransform> ().localPosition = new Vector3 (258, numberOfEntries * (-10), 0);
+		}
+		numberOfEntries++;
+	}
+		
+}
