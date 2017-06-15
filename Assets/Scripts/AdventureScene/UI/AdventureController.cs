00ce11566f24ee1d111485ba9214449a41147013
Toma Alexandru <bartimaeus1073@gmail.com>
2017-06-15 15:00:04 +0100

    Changed stuff

--- Assets/Scripts/AdventureScene/UI/AdventureController.cs
+++ Assets/Scripts/AdventureScene/UI/AdventureController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class AdventureController : Photon.PunBehaviour {
+public class AdventureController : Photon.MonoBehaviour {
 
 	public GameObject loadingScreen;
 	public GameObject party;
