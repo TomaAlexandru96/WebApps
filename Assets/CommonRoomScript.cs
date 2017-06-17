4ad585acb2ae9ebb480c957888b3a857b4f0051a
unknown <isailaandrei.i@gmail.com>
2017-06-17 11:23:56 +0100

    Done coursework questions and integration. Printers and SAO not yet done

---
+++ Assets/CommonRoomScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommonRoomScript : MonoBehaviour {
+
+	public GameObject courseworkPanel;
+	public bool doneCoursework;
+
+	public void Start () {
+		courseworkPanel.SetActive (false);
+		doneCoursework = false;
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (!doneCoursework) {
+			courseworkPanel.SetActive (true);
+			doneCoursework = true;
+		} else {
+			Debug.Log ("gets here");
+		}
+	}
+
+}
