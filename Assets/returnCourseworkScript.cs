13168a21df121f5afa96162626b7c4af2dfb157e
Vinamra Agrawal <va1215@ic.ac.uk>
2017-06-17 17:12:57 +0100

    exit after submission

--- Assets/returnCourseworkScript.cs
+++ Assets/returnCourseworkScript.cs
@@ -17,8 +17,8 @@ public class returnCourseworkScript : MonoBehaviour {
 	private IEnumerator DisplayMessage () {
 		directionPanel.transform.GetComponent<DirectionPanel> ().DisplayText ("Huh, you made it in time ! Nice Work !");
 		yield return new WaitForSeconds (2f);
-		directionPanel.transform.GetComponent<DirectionPanel> ().DisplayText ("It looks like your coursework has already been marked, go back to the common room to see what grade you got ! You don't need to hurry this time, it's all good");
-		yield return new WaitForSeconds (2f);
+		directionPanel.transform.GetComponent<DirectionPanel> ().DisplayText ("It looks like your coursework has already been marked, go back to the common room to see what grade you got!");
+		yield return new WaitForSeconds (4f);
 
 		directionPanel.SetActive (false);
 
